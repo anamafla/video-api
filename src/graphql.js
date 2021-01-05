0e6c802867d3d6304063752922cfd9f7a8cd716e
Ana Mafla <anamafla@hotmail.com>
2021-01-05 16:02:09 -0500

    graphql

--- src/graphql.js
+++ src/graphql.js
@@ -1,6 +1,6 @@
 const { ApolloServer } = require("apollo-server-lambda");
 
-const { typeDefs, resolvers } = require("./graphql");
+const { typeDefs, resolvers } = require("./graphql/index");
 
 const server = new ApolloServer({
   typeDefs,
