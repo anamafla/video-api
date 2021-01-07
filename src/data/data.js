c0d74b0a3747962e20d0d9d041ee095adc3ff266
Ana Mafla <anamafla@hotmail.com>
2021-01-07 11:17:55 -0500

    data

--- src/data/data.js
+++ src/data/data.js
@@ -32,5 +32,71 @@ module.exports = [
     thumb:
       "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/images/TearsOfSteel.jpg",
     title: "Tears of Steel"
+  },
+
+  {
+    id: "4",
+    description: "The first Blender Open Movie from 2006",
+    sources:
+      "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4",
+    subtitle: "By Blender Foundation",
+    thumb:
+      "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/images/ElephantsDream.jpg",
+    title: "Elephant Dream"
+  },
+  {
+    id: "5",
+    description:
+      "HBO GO now works with Chromecast -- the easiest way to enjoy online video on your TV. For when you want to settle into your Iron Throne to watch the latest episodes. For $35.\nLearn how to use Chromecast with HBO GO and more at google.com/chromecast.",
+    sources:
+      "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4",
+    subtitle: "By Google",
+    thumb:
+      "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/images/ForBiggerBlazes.jpg",
+    title: "For Bigger Blazes"
+  },
+  {
+    id: "6",
+    description:
+      "Introducing Chromecast. The easiest way to enjoy online video and music on your TV—for when Batman's escapes aren't quite big enough. For $35. Learn how to use Chromecast with Google Play Movies and more at google.com/chromecast.",
+    sources:
+      "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4",
+    subtitle: "By Google",
+    thumb:
+      "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/images/ForBiggerEscapes.jpg",
+    title: "For Bigger Escape"
+  },
+  {
+    id: "7",
+    description:
+      "Introducing Chromecast. The easiest way to enjoy online video and music on your TV. For $35.  Find out more at google.com/chromecast.",
+    sources:
+      "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4",
+    subtitle: "By Google",
+    thumb:
+      "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/images/ForBiggerFun.jpg",
+    title: "For Bigger Fun"
+  },
+  {
+    id: "8",
+    description:
+      "Introducing Chromecast. The easiest way to enjoy online video and music on your TV—for the times that call for bigger joyrides. For $35. Learn how to use Chromecast with YouTube and more at google.com/chromecast.",
+    sources:
+      "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
+    subtitle: "By Google",
+    thumb:
+      "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/images/ForBiggerJoyrides.jpg",
+    title: "For Bigger Joyrides"
+  },
+  {
+    id: "9",
+    description:
+      "Introducing Chromecast. The easiest way to enjoy online video and music on your TV—for when you want to make Buster's big meltdowns even bigger. For $35. Learn how to use Chromecast with Netflix and more at google.com/chromecast.",
+    sources:
+      "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerMeltdowns.mp4",
+    subtitle: "By Google",
+    thumb:
+      "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/images/ForBiggerMeltdowns.jpg",
+    title: "For Bigger Meltdowns"
   }
 ];
