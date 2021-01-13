1db479b6b81e8e867069f2df5bb366c05deaad27
Ana Mafla <anamafla@hotmail.com>
2021-01-13 10:40:55 -0500

    cors configuration

--- src/graphql.js
+++ src/graphql.js
@@ -3,10 +3,16 @@ const { ApolloServer } = require("apollo-server-lambda");
 const { typeDefs, resolvers } = require("./graphql/index");
 
 const server = new ApolloServer({
+  origin: "*",
+  credentials: true,
   typeDefs,
   resolvers,
   playground: true,
-  introspection: true,
-  cors: { origin: "*", credentials: true }
+  introspection: true
 });
-exports.handler = server.createHandler();
+exports.handler = server.createHandler({
+  cors: {
+    origin: "*",
+    credentials: true
+  }
+});
